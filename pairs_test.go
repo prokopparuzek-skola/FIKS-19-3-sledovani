b7485f8a0cce356dbf137329a7680cbcd0cd5174
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2020-01-03 20:05:57 +0100

    testy

---
+++ pairs_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+import "math/big"
+import "fmt"
+
+type pairsTest struct {
+	citisens uint64
+	numPairs uint64
+}
+
+func TestPairs(t *testing.T) {
+	var testInput []pairsTest = []pairsTest{
+		{1, 0},
+		{4, 3},
+		{25, 17},
+		{8, 5},
+		{17, 11},
+		{20, 13},
+		{5, 3},
+		{9, 6},
+		{7, 5},
+	}
+	for _, in := range testInput {
+		result := pairs(big.NewInt(int64(in.citisens)))
+		if result.Uint64() != in.numPairs {
+			msg := fmt.Sprintf("Pro %d mělo být %d ne %d", in.citisens, in.numPairs, result)
+			t.Error(msg)
+		}
+	}
+}
